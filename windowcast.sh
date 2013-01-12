9147ba177e33e97c214773b36b2383258c4e4e0d
Toksyuryel <toksyuryel@crimsondragonsoftware.com>
2013-01-12 05:13:57 -0800

    Some applications lie to xwininfo about their geometry, but the true information is still available.

--- windowcast.sh
+++ windowcast.sh
@@ -19,7 +19,7 @@ die() {
 [[ -z $QUALITY ]] && QUALITY=10
 
 INFO=$(xwininfo)
-WIN_GEO=$(echo $INFO | grep -oEe 'geometry [0-9]+x[0-9]+' | grep -oEe '[0-9]+x[0-9]+')
+WIN_GEO="$(echo $INFO | grep -oEe 'Width: [0-9]*' | grep -oEe '[0-9]*')x$(echo $INFO | grep -oEe 'Height: [0-9]*' | grep -oEe '[0-9]*')"
 WIN_XY=$(echo $INFO | grep -oEe 'Corners:\s+\+[0-9]+\+[0-9]+' | grep -oEe '[0-9]+\+[0-9]+' | sed -e 's/\+/,/')
 JACK=$(pgrep jackd)
 
