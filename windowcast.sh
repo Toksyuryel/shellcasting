dbe974470980225de8b19625b6702fcdbfa51a86
Simon Flavelle <me@neurario.com>
2013-01-16 10:20:32 +1100

    add --norm to sox parameters, fixes halved volume
    By default sox divides the volume by the number of files mixed in order to prevent clipping. Adding --norm to its parameters will normalise the volume while preventing clipping

--- windowcast.sh
+++ windowcast.sh
@@ -74,7 +74,7 @@ post_process() {
     [[ $(ffprobe -i $RECDIR/rec.mkv -show_streams -loglevel quiet | grep -c index) -eq 3 ]] || return 1
     ffmpeg -f lavfi -i "amovie=$RECDIR/rec.mkv:si=1,volume=-8dB" -y $RECDIR/audio.flac &>> $LOGDIR/ffmpeg.log
     ffmpeg -i $RECDIR/rec.mkv -map 0:0 -y $RECDIR/mic.flac &>> $LOGDIR/ffmpeg.log
-    sox -m $RECDIR/mic.flac $RECDIR/audio.flac $RECDIR/mixedaudio.flac
+    sox --norm -m $RECDIR/mic.flac $RECDIR/audio.flac $RECDIR/mixedaudio.flac
     ffmpeg -i $RECDIR/mixedaudio.flac -i $RECDIR/rec.mkv -map 0 -map 1:2 -acodec copy -vcodec copy -y $RECDIR/processed.mkv &>> $LOGDIR/ffmpeg.log
     rm -f $RECDIR/{mic,audio,mixedaudio}.flac
     echo "Post-processing complete."
