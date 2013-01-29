ae7309746454ee0cbad29f28a573117ed6915f15
Toksyuryel <toksyuryel@crimsondragonsoftware.com>
2013-01-29 05:31:35 -0800

    Since ffmpeg is so commited to the mic stream being 0:1, it is probably safe to assume this is the case in post-processing. This is only temporary until I split the mic recording to be separate like the system audio recording is.

--- screencast.sh
+++ screencast.sh
@@ -23,7 +23,7 @@ start_recording() {
     GEO=$(xdpyinfo -display :0.0 | grep -oEe 'dimensions:\s+[0-9]+x[0-9]+' | grep -oEe '[0-9]+x[0-9]+')
     FFMPEG="ffmpeg"
     [[ -n $MICSOURCE ]] && FFMPEG="$FFMPEG -f alsa -ac $MICCHANNELS -i $MICSOURCE"
-    FFMPEG="$FFMPEG -f x11grab -r $FPS -s $GEO -i :0.0 -vcodec libx264 -preset ultrafast -crf $QUALITY -map 0 -map 1 -y $RECDIR/rec.mkv &> $LOGDIR/ffmpeg.log &"
+    FFMPEG="$FFMPEG -f x11grab -r $FPS -s $GEO -i :0.0 -vcodec libx264 -preset ultrafast -crf $QUALITY -y $RECDIR/rec.mkv &> $LOGDIR/ffmpeg.log &"
 
     [[ -z $DEBUG ]] || echo $FFMPEG
     eval $FFMPEG
@@ -72,7 +72,7 @@ post_process() {
     ffmpeg -f lavfi -i "amovie=$RECDIR/audio.wav,volume=-8dB" -y $RECDIR/audio.flac &>> $LOGDIR/ffmpeg.log
     if [[ $(ffprobe -i $RECDIR/rec.mkv -show_streams -loglevel quiet | grep -c index) -eq 2 ]]
     then
-        ffmpeg -i $RECDIR/rec.mkv -map 0:0 -y $RECDIR/mic.flac &>> $LOGDIR/ffmpeg.log
+        ffmpeg -i $RECDIR/rec.mkv -map 0:1 -y $RECDIR/mic.flac &>> $LOGDIR/ffmpeg.log
         if [[ $(ffprobe -i $RECDIR/mic.flac -show_streams -loglevel quiet | grep channels | grep -oEe '[0-9]') -eq 1 ]]
         then
             sox -M $RECDIR/mic.flac $RECDIR/mic.flac $RECDIR/stereomic.flac || die "Failed to transform mic audio from mono to stereo."
